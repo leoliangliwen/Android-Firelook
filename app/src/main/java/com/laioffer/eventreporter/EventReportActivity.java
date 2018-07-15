8eb837ee9c47ce7556656651815b676d31c754d1
leo <leoliangliwen@gmail.com>
2018-07-15 13:09:36 -0400

    finished 6.9

--- app/src/main/java/com/laioffer/eventreporter/EventReportActivity.java
+++ app/src/main/java/com/laioffer/eventreporter/EventReportActivity.java
@@ -2,11 +2,18 @@ package com.laioffer.eventreporter;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -19,6 +26,22 @@ public class EventReportActivity extends Activity {
     private ImageView mImageViewSend;
     private ImageView mImageViewCamera;
     private DatabaseReference database;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +61,31 @@ public class EventReportActivity extends Activity {
                 String key = uploadEvent();
             }
         });
+
+        //auth
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+
+        mAuth.signInAnonymously().addOnCompleteListener(this,  new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d(TAG, "signInAnonymously:onComplete:" + task.isSuccessful());
+                if (!task.isSuccessful()) {
+                    Log.w(TAG, "signInAnonymously", task.getException());
+                }
+            }
+        });
+
     }
 
     private String uploadEvent() {
@@ -59,7 +107,7 @@ public class EventReportActivity extends Activity {
         event.setId(key);
         database.child("events").child(key).setValue(event, new DatabaseReference.CompletionListener() {
             @Override
-            public void onComplete(DatagitbaseError databaseError, DatabaseReference databaseReference) {
+            public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
                 if (databaseError != null) {
                     Toast toast = Toast.makeText(getBaseContext(),
                             "The event is failed, please check your network status.", Toast.LENGTH_SHORT);
