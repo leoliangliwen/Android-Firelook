24c55a20fff940ffdddefe1cf1905560fdd74e8a
leo <leoliangliwen@gmail.com>
2018-07-25 00:29:37 -0400

    finished july22 before 7

--- app/src/main/java/com/laioffer/eventreporter/EventReportActivity.java
+++ app/src/main/java/com/laioffer/eventreporter/EventReportActivity.java
@@ -1,6 +1,9 @@
 package com.laioffer.eventreporter;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.util.Log;
@@ -10,6 +13,8 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -17,6 +22,12 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EventReportActivity extends Activity {
     private static final String TAG = EventReportActivity.class.getSimpleName();
@@ -28,6 +39,13 @@ public class EventReportActivity extends Activity {
     private DatabaseReference database;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private LocationTracker mLocationTracker;
+    private FirebaseStorage storage;
+    private StorageReference storageRef;
+    private static int RESULT_LOAD_IMAGE = 1;
+    private ImageView img_event_picture;
+    private Uri mImgUri;
+
 
     @Override
     public void onStart() {
@@ -54,11 +72,20 @@ public class EventReportActivity extends Activity {
         mImageViewCamera = (ImageView) findViewById(R.id.img_event_camera);
         mImageViewSend = (ImageView) findViewById(R.id.img_event_report);
         database = FirebaseDatabase.getInstance().getReference();
+        img_event_picture = (ImageView) findViewById(R.id.img_event_picture_capture);
+
+        //Initialize cloud storage
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReference();
 
         mImageViewSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String key = uploadEvent();
+                if (mImgUri != null) {
+                    uploadImage(key);
+                    mImgUri = null;
+                }
             }
         });
 
@@ -86,6 +113,64 @@ public class EventReportActivity extends Activity {
             }
         });
 
+        // check if GPS enabled
+        mLocationTracker = new LocationTracker(this);
+        mLocationTracker.getLocation();
+        final double latitude = mLocationTracker.getLatitude();
+        final double longitude = mLocationTracker.getLongitude();
+
+        new AsyncTask<Void, Void, Void>() {
+            private List<String> mAddressList = new ArrayList<String>();
+
+            @Override
+            protected Void doInBackground(Void... urls) {
+                mAddressList = mLocationTracker.getCurrentLocationViaJSON(latitude,longitude);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void input) {
+                if (mAddressList.size() >= 3) {
+                    mEditTextLocation.setText(mAddressList.get(0) + ", " + mAddressList.get(1) +
+                            ", " + mAddressList.get(2) + ", " + mAddressList.get(3));
+                }
+            }
+        }.execute();
+
+        //Add click listener for the image to pick up images from gallery through implicit intent
+        mImageViewCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, RESULT_LOAD_IMAGE);
+            }
+        });
+
+
+    }
+
+    /**
+     * Send intent to launch gallery for us to pick up images, once the action finishes, images
+     * will be returns as parameters in this function
+     * @param requestCode code for intent to start gallery activity
+     * @param resultCode result code returned when finishing picking up images from gallery
+     * @param data content returned from gallery, including images we picked
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+                Uri selectedImage = data.getData();
+                img_event_picture.setVisibility(View.VISIBLE);
+                img_event_picture.setImageURI(selectedImage);
+                mImgUri = selectedImage;
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
     private String uploadEvent() {
@@ -118,10 +203,41 @@ public class EventReportActivity extends Activity {
                     mEditTextTitle.setText("");
                     mEditTextLocation.setText("");
                     mEditTextContent.setText("");
+                    img_event_picture.setVisibility(View.GONE);
                 }
             }
         });
         return key;
+    }
+
+    /**
+     * Upload image picked up from gallery to Firebase Cloud storage
+     * @param eventId eventId
+     */
+    private void uploadImage(final String eventId) {
+        if (mImgUri == null) {
+            return;
+        }
+        StorageReference imgRef = storageRef.child("images/" + mImgUri.getLastPathSegment() + "_"  + System.currentTimeMillis());
+
+        UploadTask uploadTask = imgRef.putFile(mImgUri);
+
+        // Register observers to listen for when the download is done or if it fails
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                @SuppressWarnings("VisibleForTests")
+                Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                Log.i(TAG, "upload successfully" + eventId);
+                database.child("events").child(eventId).child("imgUri").
+                        setValue(downloadUrl.toString());
+            }
+        });
     }
 
 }
