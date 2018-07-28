8dd0fea8159c910212bfbbef0bda7d174f14a5f9
leo <leoliangliwen@gmail.com>
2018-07-28 12:35:53 -0400

    finished 2.8

--- app/src/main/java/com/laioffer/eventreporter/EventListAdapter.java
+++ app/src/main/java/com/laioffer/eventreporter/EventListAdapter.java
@@ -1,5 +1,7 @@
 package com.laioffer.eventreporter;
 
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +9,11 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.laioffer.eventreporter.Event;
 import com.laioffer.eventreporter.R;
 
@@ -14,13 +21,14 @@ import java.util.List;
 
 public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.ViewHolder> {
     private List<Event> eventList;
-
+    private DatabaseReference databaseReference;
     /**
      * Constructor for EventListAdapter
      * @param events events that are showing on screen
      */
     public EventListAdapter(List<Event> events) {
         eventList = events;
+        databaseReference = FirebaseDatabase.getInstance().getReference();
     }
 
     /**
@@ -35,6 +43,10 @@ public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.View
         public TextView time;
         public ImageView imgview;
         public View layout;
+        public ImageView img_view_good;
+        public ImageView img_view_comment;
+        public TextView good_number;
+        public TextView comment_number;
 
         public ViewHolder(View v) {
             super(v);
@@ -44,6 +56,10 @@ public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.View
             description = (TextView) v.findViewById(R.id.event_item_description);
             time = (TextView) v.findViewById(R.id.event_item_time);
             imgview = (ImageView) v.findViewById(R.id.event_item_img);
+            img_view_good = (ImageView) v.findViewById(R.id.event_good_img);
+            img_view_comment = (ImageView) v.findViewById(R.id.event_comment_img);
+            good_number = (TextView) v.findViewById(R.id.event_good_number);
+            comment_number = (TextView) v.findViewById(R.id.event_comment_number);
         }
     }
 
@@ -59,7 +75,53 @@ public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.View
         String[] locations = event.getAddress().split(",");
         holder.location.setText(locations[1] + "," + locations[2]);
         holder.description.setText(event.getDescription());
-        holder.time.setText(String.valueOf(event.getTime()));
+        holder.time.setText(Utils.timeTransformer(event.getTime()));
+        holder.good_number.setText(String.valueOf(event.getLike()));
+
+        if (event.getImgUri() != null) {
+            final String url = event.getImgUri();
+            holder.imgview.setVisibility(View.VISIBLE);
+            new AsyncTask<Void, Void, Bitmap>(){
+                @Override
+                protected Bitmap doInBackground(Void... params) {
+                    return Utils.getBitmapFromURL(url);
+                }
+
+                @Override
+                protected void onPostExecute(Bitmap bitmap) {
+                    holder.imgview.setImageBitmap(bitmap);
+                }
+            }.execute();
+        } else {
+            holder.imgview.setVisibility(View.GONE);
+        }
+
+        //When user likes the event, push like number to firebase database
+        holder.img_view_good.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databaseReference.child("events").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            Event recordedevent = snapshot.getValue(Event.class);
+                            if (recordedevent.getId().equals(event.getId())) {
+                                int number = recordedevent.getLike();
+                                holder.good_number.setText(String.valueOf(number + 1));
+                                snapshot.getRef().child("like").setValue(number + 1);
+                                break;
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
     }
 
     /**
